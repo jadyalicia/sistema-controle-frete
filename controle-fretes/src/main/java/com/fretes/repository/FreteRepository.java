50f7f30926c8ee9eabe064d2cec1260c1834a0fb
Jady Alicia de Souza e Santos <jady.santos@donaritaembalagens.com.br>
2026-03-06 15:54:24 -0300

    Remover pasta de fretes do repositório de horas extras

--- controle-fretes/src/main/java/com/fretes/repository/FreteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.fretes.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.fretes.entity.Frete;
-
-public interface FreteRepository extends JpaRepository<Frete, Integer> {
-}
